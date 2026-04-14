ca894422546a8df57092e3cffda5639f4231f554
nitinthakurdev <radheradhe9870023@gmail.com>
2026-04-14 22:03:10 +0530

    small issue fixed v1

--- src/server.ts
+++ src/server.ts
@@ -22,11 +22,11 @@ export const start = (app: Application): void => {
 };
 
 function slanderedMiddleware(app: Application): void {
-    app.set('trust proxy', true);
+    app.set('trust proxy', 1); 
     app.use(express.json({ limit: '100mb' }));
     app.use(express.urlencoded({ extended: true, limit: '100mb' }));
     app.use(cors({
-        origin: [config.CLIENT_URL, config.LOCAL_CLIENT_URL],
+        origin: config.NODE_ENV === "development" ? config.LOCAL_CLIENT_URL : config.CLIENT_URL,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     }));
@@ -34,6 +34,7 @@ function slanderedMiddleware(app: Application): void {
         name: "session",
         keys: [`${config.SECRET_KEY_ONE}`, `${config.SECRET_KEY_TWO}`],
         maxAge: 24 * 7 * 3600000,
+        sameSite: "lax",
         secure: config.NODE_ENV !== "development",
     }));
     app.use(cookieParser());
